49dd144e027914fde8016a26c7a985f899a82f59
Chris Banta <chrisbanta1@gmail.com>
2020-03-11 22:58:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,23 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-
+  trains = {
+      :railroads => {
+        :pieces => 4
+        :rent_in_dollars => {
+          :one_piece_owned: 25,
+          :two_pieces_owned: 50,
+          :three_piece_owned: 75,
+          :four_pieces_owned: 100,
+          }
+        :names => {
+          :reading_railroad => {},
+          :pennslyvania_railroad => {},
+          :b_and_o_railroad => {},
+          :shortline_railroad => {}
+        }
+        }
+      }
 
 end
 
